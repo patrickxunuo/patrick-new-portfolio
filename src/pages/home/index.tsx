2c2028b319353cf59d713b7213eb3888154db00d
patrickxunuo <patrickxunuo@hotmail.com>
2023-08-01 21:41:15 -0700

    home page animte on show

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,53 @@
+import { motion, Variants } from "framer-motion";
 import "./styles.scss";
+
+const parentVariants: Variants = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.25,
+    },
+  },
+};
+
+const letterVariants: Variants = {
+  hidden: {
+    left: -150,
+  },
+  show: {
+    left: 0,
+    transition: {
+      duration: 0.8,
+      ease: [0.43, 0.13, 0.23, 0.96],
+    },
+  },
+};
 
 const Home = () => {
   return (
     <div className="home__container">
-      <div className="letter">P</div>
-      <div className="letter">A</div>
-      <div className="letter">T</div>
-      <div className="letter">X</div>
+      <motion.div
+        className="home__content"
+        variants={parentVariants}
+        initial="hidden"
+        animate="show"
+      >
+        <div className="letter">
+          <motion.span variants={letterVariants}>P</motion.span>
+        </div>
+        <div className="letter">
+          <motion.span variants={letterVariants}>A</motion.span>
+        </div>
+        <div className="letter">
+          <motion.span variants={letterVariants}>T</motion.span>
+        </div>
+        <div className="letter">
+          <motion.span variants={letterVariants}>X</motion.span>
+        </div>
+      </motion.div>
     </div>
   );
 };
